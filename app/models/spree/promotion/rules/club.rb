41723277500bca1c3f56fa7660ebbcfb8318a81d
Enol Iglesias <enoliglesias@gmail.com>
2016-02-01 12:23:03 +0100

    Aplicar ventajas del club si usuario invitado y tiene tarjeta en carrito

--- app/models/spree/promotion/rules/club.rb
+++ app/models/spree/promotion/rules/club.rb
@@ -3,7 +3,9 @@ module Spree
   class Promotion::Rules::Club < PromotionRule
 
     def eligible?(order, options = {})
-      return false if order.user.blank?
+      # 41557 si el usuario es invitado y esta comprando
+      # la tarjeta hay que aplicarle ventajas del club
+      return order.include_card? if order.user.blank?
       return false unless order.user.respond_to?(:club?)
 
       order.user.club?
